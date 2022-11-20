b3d5b6a84eae1ce113f1ba20d69a60a174a8d4e2
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-20 20:32:26 +0600

    Add files via upload

---
+++ jQuery-Calculator desing/java.js
@@ -0,0 +1,43 @@
+// function insertNumber(number){
+//    var existingNumbers =  $('#result').val()
+//    $('#result').val(existingNumbers + number)
+// }
+
+// function clearResult(){
+//    $('#result').val('')
+// }
+
+// function calculate(){
+//    var result = eval($('#result').val())
+//    $('#result').val(result)
+// }
+
+// function deleteNumber(){
+//    var presentValue = $('#result').val()
+//    if(presentValue!=''){
+//       $('#result').val(presentValue.slice(0,-1))
+//    }
+// }
+
+var resultField = $('#result')
+
+function insertNumber(number){
+   var existingNumbers =  resultField.val()
+   resultField.val(existingNumbers + number)
+}
+
+function clearResult(){
+   resultField.val('')
+}
+
+function calculate(){
+   var result = eval(resultField.val())
+   resultField.val(result)
+}
+
+function deleteNumber(){
+   var presentValue = resultField.val()
+   if(presentValue!=''){
+      resultField.val(presentValue.slice(0,-1))
+   }
+}
